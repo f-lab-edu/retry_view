3e11496c1765a0b915e44766842e8e4f1120c919
5upportPark <5upportpark7@gmail.com>
2024-10-22 15:31:03 +0900

    repository 어노테이션 추가

--- src/main/java/com/pjw/retry_view/repository/ProductRepository.java
+++ src/main/java/com/pjw/retry_view/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.pjw.retry_view.repository;
 
 import com.pjw.retry_view.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     public List<Product> findAll();
     public Optional<Product> findById(Long id);
