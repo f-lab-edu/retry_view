00a23fa3caec5ac7bf58f305417258207cd86cd1
5upportPark <5upportpark7@gmail.com>
2024-10-31 17:15:53 +0900

    transactio 추가

--- src/main/java/com/pjw/retry_view/service/BoardLikeService.java
+++ src/main/java/com/pjw/retry_view/service/BoardLikeService.java
@@ -5,6 +5,7 @@ import com.pjw.retry_view.entity.BoardLike;
 import com.pjw.retry_view.entity.LikeId;
 import com.pjw.retry_view.repository.BoardLikeRepository;
 import com.pjw.retry_view.util.JWTUtil;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,12 +23,14 @@ public class BoardLikeService {
         return boardLikeRepository.findByIdUserId(userId).stream().map(BoardLike::toDTO).toList();
     }
 
+    @Transactional
     public void saveBoardLike(Long boardId){
         Long userId = JWTUtil.getUserId();
         BoardLike like = BoardLike.newOne(userId, boardId);
         boardLikeRepository.save(like);
     }
 
+    @Transactional
     public void deleteBoardLike(Long boardId){
         Long userId = JWTUtil.getUserId();
         boardLikeRepository.deleteById(LikeId.newOne(userId, boardId));
